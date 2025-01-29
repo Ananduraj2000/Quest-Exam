a633ae62f18fcd39783def664b14b02d7cc5550c
Ananduraj2000 <ananduraj42412@gmail.com>
2025-01-29 15:40:05 +0530

    Exam submission

---
+++ app/src/main/java/com/example/assessment2/ResultActivity.kt
@@ -0,0 +1,10 @@
+package com.example.assessment2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class ResultActivity:ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
